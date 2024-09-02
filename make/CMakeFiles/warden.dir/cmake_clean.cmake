e4bcb5d08a5c66ba06852ad2192025469fbe391f
irdali <irdali.durrani@jp.honda-ri.com>
2024-09-02 18:42:16 +0900

    something

---
+++ make/CMakeFiles/warden.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/warden.dir/src/main.cpp.o"
+  "CMakeFiles/warden.dir/src/main.cpp.o.d"
+  "warden"
+  "warden.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/warden.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ make/CMakeFiles/warden.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for warden.
+# This may be replaced when dependencies are built.
